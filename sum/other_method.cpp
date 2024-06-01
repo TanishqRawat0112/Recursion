d54d54af79cb327a4cfc1737d2519550076d8be2
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-06-02 01:16:44 +0530

    using global variable

--- sum/other_method.cpp
+++ sum/other_method.cpp
@@ -1,9 +1,25 @@
-//using global variable
+//Finding sum of first n natural numbers using global variable
 
 #include<iostream>
 using namespace std;
+
+int total=0;
+
+void sum(int n){
+    if(n==0){
+        return;
+    }
+    total+=n;
+    sum(n-1);
+}
  
 int main(){
+    int n;
+    cout<<"Enter the number here : ";
+    cin>>n;
 
+    sum(n);
+
+    cout<<"The sum of first "<<n<<" natural numbers are : "<<total<<endl;
 return 0;
 }
