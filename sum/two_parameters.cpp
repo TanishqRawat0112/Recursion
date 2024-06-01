f417713d4470c10167fcdaa5bb072069d021e022
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-06-02 01:38:02 +0530

    using two parameters in the recursive function

--- sum/two_parameters.cpp
+++ sum/two_parameters.cpp
@@ -1,1 +1,21 @@
-//Finding the sum of first n 
+//Finding the sum of first n natural numbers using two parameters n a recursive function.
+
+#include<iostream>
+using namespace std;
+
+int sum2(int n,int sum){
+    if(n==0){
+        return sum;
+    }
+    return sum2(n-1,sum+n);
+};
+ 
+int main(){
+    int n;
+    cout<<"Enter the number here : ";
+    cin>>n;
+
+    int ans=sum2(n,0);
+    cout<<"The sum is : "<<ans<<endl;
+return 0;
+}
