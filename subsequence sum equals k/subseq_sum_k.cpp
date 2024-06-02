818ad60e83f4f5e54847a9f3befbadea4dfca6a8
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-06-03 00:29:04 +0530

    added a function which finds just one subsequence whose sum equals k

--- subsequence sum equals k/subseq_sum_k.cpp
+++ subsequence sum equals k/subseq_sum_k.cpp
@@ -3,7 +3,7 @@
 using namespace std;
  //Recursive function when the array contains only positives
 void subsequence_sum_equals_k(vector<int>&arr,int index,int size,int k,int sum,vector<vector<int> >&answer,vector<int>&list){
-    if(sum==k || index==size){
+    if(sum>=k || index==size){
         if(sum==k){
             answer.push_back(list);
         }
@@ -28,6 +28,21 @@ void subsequence_sum_equals_k2(vector<int>&arr,int index,int size,int k,int sum,
     list.pop_back();
     subsequence_sum_equals_k2(arr,index+1,size,k,sum,answer,list); 
 }
+//Function to find only one subsequence whose sum equals k.
+void only_subsequence_sum_equals_k(vector<int>&arr,int index,int size,int k,int sum,vector<vector<int> >&answer,vector<int>&list){
+    if(index==size){  
+        if(answer.size()==0){
+            if(sum==k){
+                answer.push_back(list);
+            }
+        }
+        return;
+    }
+    list.push_back(arr[index]);
+    only_subsequence_sum_equals_k(arr,index+1,size,k,sum+arr[index],answer,list);
+    list.pop_back();
+    only_subsequence_sum_equals_k(arr,index+1,size,k,sum,answer,list); 
+}
 
 int main(){
     int n;
@@ -48,7 +63,7 @@ int main(){
 
     vector<int>list;
     vector<vector<int> > answer;
-    subsequence_sum_equals_k2(arr,0,n,k,0,answer,list);
+    only_subsequence_sum_equals_k(arr,0,n,k,0,answer,list);
 
     for(auto it:answer){
         for(auto i:it){
