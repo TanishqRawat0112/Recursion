c9fb31f23a8824b5b37b606c7badd0ebe71294b7
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-06-03 00:47:26 +0530

    count number of subsequences whose sum equals k

--- subsequence sum equals k/subseq_sum_k.cpp
+++ subsequence sum equals k/subseq_sum_k.cpp
@@ -44,6 +44,45 @@ void only_subsequence_sum_equals_k(vector<int>&arr,int index,int size,int k,int 
     only_subsequence_sum_equals_k(arr,index+1,size,k,sum,answer,list); 
 }
 
+//Using functional recursion to print just one subsequence:
+bool subsequence_sum_equals_k3(vector<int>&arr,int index,int size,int k,int sum,vector<vector<int> >&answer,vector<int>&list){
+    if(index==size){
+        if(sum==k){
+            answer.push_back(list);
+            return true;
+        }
+        return false;
+    }
+    list.push_back(arr[index]);
+    if(subsequence_sum_equals_k3(arr,index+1,size,k,sum+arr[index],answer,list)==true){
+        return true;
+    }
+    list.pop_back();
+    if(subsequence_sum_equals_k3(arr,index+1,size,k,sum,answer,list)==true){
+        return true;
+    }
+
+    return false; 
+}
+
+//function that returns number of subsequences
+//Using functional recursion to print just one subsequence:
+int subsequence_sum_equals_k4(vector<int>&arr,int index,int size,int k,int sum,vector<vector<int> >&answer,vector<int>&list){
+    if(index==size){
+        if(sum==k){
+            answer.push_back(list);
+            return 1;
+        }
+        return 0;
+    }
+    list.push_back(arr[index]);
+    int left = subsequence_sum_equals_k4(arr,index+1,size,k,sum+arr[index],answer,list);
+    list.pop_back();
+    int right = subsequence_sum_equals_k4(arr,index+1,size,k,sum,answer,list);
+
+    return left + right; 
+}
+
 int main(){
     int n;
     cout<<"Enter the size of the array : ";
@@ -63,14 +102,16 @@ int main(){
 
     vector<int>list;
     vector<vector<int> > answer;
-    only_subsequence_sum_equals_k(arr,0,n,k,0,answer,list);
+    int count=subsequence_sum_equals_k4(arr,0,n,k,0,answer,list);
+    cout<<"The number of subsequences are : "<<count<<endl;
+    // subsequence_sum_equals_k3(arr,0,n,k,0,answer,list);
 
-    for(auto it:answer){
-        for(auto i:it){
-            cout<<i<<" ";
-        }
-        cout<<endl;
-    }
+    // for(auto it:answer){
+    //     for(auto i:it){
+    //         cout<<i<<" ";
+    //     }
+    //     cout<<endl;
+    // }
 
 return 0;
 }
