788af1053e895c08d8ffc00b64684be88c9e4981
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-06-03 00:10:35 +0530

    Recursive implementation to find all subsequences whose sum equals k for both positives and all real numbers

---
+++ subsequence sum equals k/subseq_sum_k.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ //Recursive function when the array contains only positives
+void subsequence_sum_equals_k(vector<int>&arr,int index,int size,int k,int sum,vector<vector<int> >&answer,vector<int>&list){
+    if(sum==k || index==size){
+        if(sum==k){
+            answer.push_back(list);
+        }
+        return;
+    }
+    list.push_back(arr[index]);
+    subsequence_sum_equals_k(arr,index+1,size,k,sum+arr[index],answer,list);
+    list.pop_back();
+    subsequence_sum_equals_k(arr,index+1,size,k,sum,answer,list); 
+}
+
+//recursive function when the array contains positives,zeroes and negatives
+void subsequence_sum_equals_k2(vector<int>&arr,int index,int size,int k,int sum,vector<vector<int> >&answer,vector<int>&list){
+    if(index==size){
+        if(sum==k){
+            answer.push_back(list);
+        }
+        return;
+    }
+    list.push_back(arr[index]);
+    subsequence_sum_equals_k2(arr,index+1,size,k,sum+arr[index],answer,list);
+    list.pop_back();
+    subsequence_sum_equals_k2(arr,index+1,size,k,sum,answer,list); 
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
+
+    vector<int>arr;
+    int inp;
+    cout<<"Enter the array here : ";
+    for(int i=0;i<n;i++){
+        cin>>inp;
+        arr.push_back(inp);
+    }
+
+    int k;
+    cout<<"Enter the number to which the sum should be equal : ";\
+    cin>>k;
+
+    vector<int>list;
+    vector<vector<int> > answer;
+    subsequence_sum_equals_k2(arr,0,n,k,0,answer,list);
+
+    for(auto it:answer){
+        for(auto i:it){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+return 0;
+}
