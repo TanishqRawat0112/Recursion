ec9f9369938f74b701d8eb5052b5c52d3867bedd
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-06-12 10:52:47 +0530

    .

--- subset sum/subset_sum.cpp
+++ subset sum/subset_sum.cpp
@@ -3,6 +3,53 @@
 #include<algorithm>
 using namespace std;
 
+void merge(int arr[],int start,int mid,int end){
+    int n=mid-start+1;
+    int m=end-mid;
+    int arr1[n];
+    int arr2[m];
+    for(int i=0;i<n;i++){
+        arr1[i]=arr[i+start];
+    }
+    for(int i=0;i<m;i++){
+        arr2[i]=arr[i+mid+1];
+    }
+    int i=0,j=0;
+    int index=start;
+    while(i<n && j<m){
+        if(arr1[i]<=arr2[j]){
+            arr[index]=arr1[i];
+            i++;
+        }
+        else{
+            arr[index]=arr2[j];
+            j++;
+        }
+        index++;
+    }
+    while(i<n){
+        arr[index]=arr1[i];
+        i++;
+        index++;
+    }
+    while(j<m){
+        arr[index]=arr2[j];
+        j++;
+        index++;
+    } 
+}
+
+void merge_sort(int arr[],int start,int end){
+    if(start>=end){
+        return;
+    }
+    int mid=(start+end)/2;
+    merge_sort(arr,start,mid);
+    merge_sort(arr,mid+1,end);
+
+    merge(arr,start,mid,end);
+}
+
 void subset_sum(int arr[],int size,int index,int sum,vector<int>&ans){
     if(index>size){
         ans.push_back(sum);
@@ -31,9 +78,16 @@ int main(){
     subset_sum(arr,n-1,0,0,ans);
 
     // ans.sort(ans.begin(),ans.end());
+    int arr_size=pow(2,n);
+    int final[arr_size];
 
-    for(auto it:ans){
-        cout<<it<<" ";
+    for(int i=0;i<arr_size;i++){
+        final[i]=ans[i];
+    }
+    merge_sort(final,0,arr_size-1);
+
+    for(int i=0;i<arr_size;i++){
+        cout<<final[i]<<" ";
     }
     cout<<endl;
 return 0;
